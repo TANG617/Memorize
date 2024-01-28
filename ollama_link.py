a2371ca1f95640bea50a45f15a294ff1d54d8938
LiTang <litang0617@outlook.com>
2024-01-28 13:43:40 +0800

    works

--- ollama_link.py
+++ ollama_link.py
@@ -5,18 +5,14 @@ from ollama import Client
 
 
 client = Client(host='http://api.tritium.top:11434',timeout=1000)
-def llama_link(word):
+def llama_rank(word):
     response = client.chat(model='llama2', messages=[
           {
             'role': 'user',
-            # 'prompt': 'you are a judge aimed at sorting the following English vocabulary into 5 classes based on the difficulties of people memorizing. '
-            #           'I will give you the word and you should give me the number from 1 to 5, '
-            #           'in which, 1 is the easiest and 5 is the hardest',
-
             'content': 'you are a judge aimed at mark the following English word into 10 classes based on the difficulties for university student to memorize '
                         'I will give you the word and you should give me the number from 0 to 9, in which, 0 is the easiest and 9 is the hardest'
                         'for example , if I give you word <apple> you should answer me <Level 0>'
-                        # 'if you understood, do not reply any word and just begin the task:'
+                        'if I give you word <groa> you should answer me <Level 8>'
 
           },
         {
@@ -32,7 +28,7 @@ def llama_link(word):
 
     pattern = r'[0-9]'
     ans = response['message']['content']
-    print(ans)
+    # print(ans)
     matches = re.findall(pattern, ans)
 
     try:
@@ -41,3 +37,47 @@ def llama_link(word):
     except:
         print("ERROR")
         return 0
+
+def llama_sentence(word):
+    response = client.chat(model='llama2', messages=[
+          {
+            'role': 'user',
+            'content': 'you are a native English speaker. I need you to make a sentence of word. '
+                       'Do not answer anything except the sentence.'
+                       'for example, the word is \'content\' you should answer me like  \' She felt content after finishing her favorite meal.\''
+          },
+        {
+            'role': 'assistant',
+            'content': 'Of course! I\'d be happy to help. '
+                       'Please provide the English word and I would make a sentence directly without any other prompts'
+
+        },
+        {
+            'role': 'user',
+            'content':'the word is \"' + word + '\", please make a sentence with the word'
+        }
+    ])
+
+    pattern = r'\n\s*(.*)'
+    ans = response['message']['content']
+    # print(ans)
+    try:
+        match = re.search(pattern, ans)[0]
+        # match.replace("\n" , ' ')
+        # match.removesuffix('\n\n')
+        # match[3]
+        print(match[2:])
+        return match[2:]
+    except:
+        print(ans)
+        return ans
+
+# llama_sentence("covin")
+    # matches = re.findall(pattern, ans)
+    #
+    # try:
+    #     print("【" + word + ":" + matches[0] + "】")
+    #     return matches[0]
+    # except:
+    #     print("ERROR")
+    #     return 0
